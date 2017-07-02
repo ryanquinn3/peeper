e34d73fefb3a77455ad92692f66b4b5fac770b46
Ryan Quinn <ryan3q@gmail.com>
2017-07-02 16:32:53 -0700

    sheet class

--- src/sheets.js
+++ src/sheets.js
@@ -1,4 +1,5 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
+const { omit, map } = require('ramda');
 
 const { promisify } = require('util');
 
@@ -7,29 +8,49 @@ const sheetId = '1EvjG_nq2lQBbVS89c22-rFXdyq0FcTldq-zsQd5YtIY';
 const sheet = new GoogleSpreadsheet(sheetId);
 
 const useServiceAccountAuth = promisify(sheet.useServiceAccountAuth);
-
 const getInfo = promisify(sheet.getInfo);
 
+const removeXml = map(omit('_xml'));
+
+class Sheet {
+  constructor(title){
+    this.title = title;
+    this.instance = null;
+  }
+
+  async connect(){
+    if(this.instance) {
+      return this.instance;
+    }
+    await useServiceAccountAuth(require('../google_keys.json'));
+    const { worksheets } = await getInfo();
+    const instance = worksheets.find((sheet) => {
+      return sheet.title = this.title
+    });
+    if(!instance){
+      throw new Error('No sheet found with that title');
+    }
+    this.instance = instance;
+    this.instance.getRows = promisify(instance.getRows);
+    this.instance.addRow = promisify(instance.addRow);
+    return this.instance;
+  }
+
+  async getRows(rowOptions = { offset: 0 , limit: 100 }){
+    await this.connect();
+    const rows = await this.instance.getRows(rowOptions);
+    return removeXml(rows);
+  }
+
+  async addRow(row){
+    await this.connect();
+    return await this.instance.addRow(row);
+  }
+
+}
+
 (async() => {
-  await useServiceAccountAuth(require('../google_keys.json'));
-  const info = await getInfo();
-
-  const [ worksheet ] = info.worksheets;
-
-
-  const getRows = promisify(worksheet.getRows);
-
-
-  const rows = await getRows({ offset: 0 , limit: 100 });
-  
-  rows.forEach((row) => {
-    delete row._xml;
-  })
-
-  console.log(`Total rows returned: ${rows.length}`); 
-  console.log(JSON.stringify(rows));
-
-  //  const rows = await getRows({ offset: 1, limit: 100 });
-
-
+  const resultsSheet = new Sheet('results');
+  const rows = await resultsSheet.getRows();
+  console.log(rows);
 })();
